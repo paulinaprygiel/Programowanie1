5827321e349be56c7ac5b67e390c8817aad8aa2f
paulinaprygiel <paaaula545@gmail.com>
2021-03-29 14:24:50 +0200

    add zad3

--- Zestaw2/zad3.cpp
+++ Zestaw2/zad3.cpp
@@ -1,4 +1,30 @@
 //
 // Created by Paulina Prygiel on 29/03/2021.
 //
+#include <iostream>
 
+float x, y, z;
+
+int main()
+{
+    std:: cout << "Podaj pierwsza liczbe" << std:: endl;
+    std:: cin >> x;
+
+    std:: cout << "Podaj druga liczbe" << std:: endl;
+    std:: cin >> y;
+
+    std:: cout << "Podaj trzecia liczbe" << std:: endl;
+    std:: cin >> z;
+
+    if ((x<=y)&&(y<=z))
+
+        std:: cout <<"Liczby sa ulozone w porzadku niemalejacym";
+
+
+    else
+
+        std:: cout<<"Liczby nie sa ulozone w porzadku niemalejacym";
+
+
+    return 0;
+}
