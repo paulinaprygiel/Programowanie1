4f95bd3b2588542442622cc97082e5ca5d1d930c
paulinaprygiel <paaaula545@gmail.com>
2021-03-29 13:57:35 +0200

    add zad1

--- Zestaw2/zad1.cpp
+++ Zestaw2/zad1.cpp
@@ -4,11 +4,20 @@
 #include <iostream>
 int main()
 
-{
+{ int a;
+std:: cout << "Wprowadz liczbe";
+std:: cin >> a;
+if (a>0)
 
+    std:: cout << "Liczba jest dodatnia";
 
+else if (a<0)
 
+    std:: cout << "Liczba jest ujemna";
 
+else
+
+    std::cout << "Podana liczba to 0";
 
 
     return 0;
