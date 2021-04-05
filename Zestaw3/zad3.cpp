a272db48b79ff95af696c78df928777f013f0112
paulinaprygiel <paaaula545@gmail.com>
2021-04-05 14:07:34 +0200

    add zad3/4

--- Zestaw3/zad3.cpp
+++ Zestaw3/zad3.cpp
@@ -5,12 +5,15 @@ int main()
     std:: cout << "Podaj liczbe calkowita";
     std:: cin >> n;
     std:: cout << "Dzielnikami liczby "<< n << " sa "<< std:: endl;
-    for (int i = n; i > 0; i--)
+    for (int i = 1; i <= n/2.0; i++)
     {
+        int dzielnik = n/i;
         if (n%i==0)
 
-            std:: cout << n/i<< std:: endl;
+            std:: cout << dzielnik <<std:: endl;
     }
+    std:: cout <<1;
 
     return 0;
 }
+
