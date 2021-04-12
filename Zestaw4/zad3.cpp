886d734eeaab1e860f8b424d6df566e7def68c51
paulinaprygiel <paaaula545@gmail.com>
2021-04-12 18:17:04 +0200

    add zad4/6

--- Zestaw4/zad3.cpp
+++ Zestaw4/zad3.cpp
@@ -1,25 +1,5 @@
 
-//#include <iostream>
-//
-//
-//using namespace std;
-//
-//int main()
-//{
-//
-//    int A[] = {1,3,5,7,9};
-//    int n;
-//    int a;
-//    cout<< "wprowadz n";
-//    cin>>n;
-//
 
-//    for(int i=0;i<5;i++)
-//    {
-//        a = A[i];
-//        A[i]= a+n;
-//        cout<<A[i]<<endl;
-//    }
 #include <iostream>
 
 
